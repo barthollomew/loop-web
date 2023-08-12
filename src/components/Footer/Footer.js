0cb58bf4bde187cabcf4ac59bac5cbf996ed9015
barthollomew <nathan.nn946@gmail.com>
2023-08-12 18:01:38 +1000

    Added styling and finished footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,11 +1,28 @@
-// Footer.js
 import React from 'react';
 import './Footer.css';
 
 const Footer = () => {
   return (
     <footer className="footer">
-      <p>© 2023 My React App. All rights reserved.</p>
+
+      {/* First Column */}
+      <div className="footer-logo-section">
+        <img src="logo2.svg" alt="Logo" className="footer-logo" />
+      </div>
+
+      {/* Second Column */}
+      <div className="footer-links-section">
+        <a className="footer-link" href="/">Home</a>
+        <a className="footer-link" href="/movies">Movies</a>
+        <a className="footer-link" href="/reviews">Reviews</a>
+        <a className="footer-link" href="/profile">Profile</a>
+      </div>
+
+      {/* Third Column */}
+      <div className="footer-social-section">
+        <p>© 2023 Loop Web. Full Stack Assignment by Nathan Nguyen and Jonathan Theofilas.</p>
+      </div>
+
     </footer>
   );
 };
