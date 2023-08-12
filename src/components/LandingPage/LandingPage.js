2fe41d79e18154c892e8eb3b1fc7359feaace20d
barthollomew <nathan.nn946@gmail.com>
2023-08-12 18:28:09 +1000

    Added gaps to landing page.

--- src/components/LandingPage/LandingPage.js
+++ src/components/LandingPage/LandingPage.js
@@ -12,7 +12,7 @@ const LandingPage = () => {
           <p className="text-xl">Experience unforgettable movie magic!</p>
         </div>
         <div className="logo-section flex flex-col items-center">
-          <img src="logo.svg" alt="Logo" className="mb-4" />
+          <img src={process.env.PUBLIC_URL + '/logos/logo2.svg'} alt="Logo" className="mb-4" />
           <button className="btn-view-movies bg-blue-500 text-white px-4 py-2 rounded-md">
             View Movies
           </button>
@@ -22,18 +22,18 @@ const LandingPage = () => {
       {/* Section 2: Carousel */}
       <div className="carousel-section flex justify-between items-center py-10">
         <div className="movie-card rounded-lg mr-4">
-          <img src="movie1.png" alt="Movie 1" className="rounded-lg" />
-          <p className="mt-2">12:30 PM, Central Cinema</p>
+          <img src={process.env.PUBLIC_URL + '/images/shrek1.jpg'} alt="Movie 1" className="rounded-lg" />
+          <p className="mt-2">12:30 PM, Melbourne Central</p>
         </div>
         <div className="movie-card rounded-lg">
-          <img src="movie1.png" alt="Movie 2" className="rounded-lg" />
-          <p className="mt-2">3:30 PM, Downtown Theater</p>
+        <img src={process.env.PUBLIC_URL + '/images/shrek2.jpg'} alt="Movie 2" className="rounded-lg" />
+          <p className="mt-2">3:30 PM, Sunshine</p>
         </div>
       </div>
 
       {/* Section 3: Promotion */}
       <div className="promo-section flex justify-center items-center py-10 bg-yellow-300">
-        <h2 className="text-2xl font-bold">Buy one get one free</h2>
+        <h2 className="text-2xl font-bold">Buy one get none free</h2>
       </div>
 
     </div>
