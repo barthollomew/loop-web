83ad97e3afdf7c811cc171f67efe2ff717ecf63e
barthollomew <nathan.nn946@gmail.com>
2023-08-17 16:20:12 +1000

    Updated links to headers

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,30 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
+import Profile from '../Profile/Profile';  
 import './Header.css';
 
 const Header = () => {
+  const [isProfileVisible, setProfileVisible] = useState(false);
+
   return (
     <div className="header">
       <div className="logo-section">
-        <Link to="/logoPage"> {/* Use Link instead of anchor tag */}
-          <img src={process.env.PUBLIC_URL + '/logos/logo.svg'} alt="Logo" className="logo" />
-        </Link>
+          <Link to="/">
+              <img src={process.env.PUBLIC_URL + '/logos/logo.svg'} alt="Logo" className="logo" />
+          </Link>
       </div>
 
       <div className="actions-section">
-        <Link to="/signIn"> {/* Use Link instead of anchor tag */}
+        <Link to="/signIn">
           <button className="btn btn-signin mr-4">Sign In</button>
         </Link>
-        <Link to="/signUp"> {/* Use Link instead of anchor tag */}
+        <Link to="/signUp">
           <button className="btn btn-signup mr-4">Sign Up</button>
         </Link>
       </div>
 
-      <div className="avatar-section">
-        <Link to="/avatarPage"> {/* Use Link instead of anchor tag */}
-          <img src={process.env.PUBLIC_URL + '/logos/avatar.svg'} alt="Avatar" className="avatar" />
-        </Link>
+      <div className="avatar-section" onClick={() => setProfileVisible(!isProfileVisible)}>
+        <img src={process.env.PUBLIC_URL + '/logos/avatar.svg'} alt="Avatar" className="avatar" />
       </div>
+
+      {/* Profile Component */}
+      {isProfileVisible && <Profile user={{ /*pass the user data here */ }} onClose={() => setProfileVisible(false)} />}
     </div>
   );
 }
