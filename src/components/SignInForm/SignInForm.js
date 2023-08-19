7b3f98366279958d8b1f3b95b5f0f007fd124f20
barthollomew <nathan.nn946@gmail.com>
2023-08-19 20:14:50 +1000

    Completed Sign in Sign Up pages with CSS

--- src/components/SignInForm/SignInForm.js
+++ src/components/SignInForm/SignInForm.js
@@ -1,36 +1,55 @@
-import React, { useState } from "react";
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 
-const SignInForm = () => {
-    const [username, setUsername] = useState("");
-    const [password, setPassword] = useState("");
 
-    const handleSignIn = (e) => {
-        e.preventDefault();
-        console.log("Username:", username);
-        console.log("Password:", password);
+const SignInPage = () => {
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+    const navigate = useNavigate();
+
+    const handleSignIn = () => {
+        const users = JSON.parse(localStorage.getItem("users") || "[]");
+        const user = users.find(user => user.username === username);
+
+        if (!user || user.password !== password) {
+            alert("Incorrect username or password");
+            return;
+        }
+
+        localStorage.setItem("currentUser", JSON.stringify(user));
+
+        alert("Successfully signed in!");
+        navigate("/Profile/ProfilePage");
     };
 
     return (
-        <form onSubmit={handleSignIn}>
-            <label htmlFor="username">Username:</label>
-            <input
-                type="text"
-                id="username"
-                name="username"
-                value={username}
-                onChange={(e) => setUsername(e.target.value)}
-            />
-            <label htmlFor="password">Password:</label>
-            <input
-                type="password"
-                id="password"
-                name="password"
-                value={password}
-                onChange={(e) => setPassword(e.target.value)}
-            />
-            <button type="submit">Sign In</button>
-        </form>
+        <div className="min-h-screen flex items-center justify-center bg-gray-100 py-12 px-4 sm:px-6 lg:px-8">
+            <div className="max-w-md w-full">
+                <div>
+                    <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">
+                        Sign in to your account
+                    </h2>
+                </div>
+                <input
+                    type="text"
+                    className="mt-2 block w-full p-3 rounded-md bg-gray-200 border-transparent focus:border-gray-500 focus:bg-white focus:ring-0"
+                    placeholder="Username"
+                    value={username}
+                    onChange={e => setUsername(e.target.value)}
+                />
+                <input
+                    type="password"
+                    className="mt-2 block w-full p-3 rounded-md bg-gray-200 border-transparent focus:border-gray-500 focus:bg-white focus:ring-0"
+                    placeholder="Password"
+                    value={password}
+                    onChange={e => setPassword(e.target.value)}
+                />
+                <button className="mt-2 w-full px-3 py-4 text-white bg-indigo-500 rounded-md focus:bg-indigo-600 focus:outline-none" onClick={handleSignIn}>
+                    Sign In
+                </button>
+            </div>
+        </div>
     );
-};
+}
 
-export default SignInForm;
+export default SignInPage;
--- src/components/SignUpForm/SignUpForm.js
+++ src/components/SignUpForm/SignUpForm.js
@@ -1,48 +1,55 @@
-// SignUpForm.js
-import React, { useState } from "react";
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 
-const SignUpForm = () => {
-    const [username, setUsername] = useState("");
-    const [email, setEmail] = useState("");
-    const [password, setPassword] = useState("");
+const SignUpPage = () => {
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+    const navigate = useNavigate();
 
-    const handleSignUp = (e) => {
-        e.preventDefault();
+    const handleSignUp = () => {
+        const users = JSON.parse(localStorage.getItem("users") || "[]");
+        if (users.some(user => user.username === username)) {
+            alert("Username already exists");
+            return;
+        }
 
-        console.log("Username:", username);
-        console.log("Email:", email);
-        console.log("Password:", password);
+        const newUser = { username, password, name: '', email: '' };
+        localStorage.setItem("users", JSON.stringify([...users, newUser]));
+        localStorage.setItem("currentUser", JSON.stringify(newUser));
+
+        alert("Successfully signed up!");
+        navigate("/Profile/ProfilePage");
     };
 
     return (
-        <form onSubmit={handleSignUp}>
-            <label htmlFor="username">Username:</label>
-            <input
-                type="text"
-                id="username"
-                name="username"
-                value={username}
-                onChange={(e) => setUsername(e.target.value)}
-            />
-            <label htmlFor="email">Email:</label>
-            <input
-                type="email"
-                id="email"
-                name="email"
-                value={email}
-                onChange={(e) => setEmail(e.target.value)}
-            />
-            <label htmlFor="password">Password:</label>
-            <input
-                type="password"
-                id="password"
-                name="password"
-                value={password}
-                onChange={(e) => setPassword(e.target.value)}
-            />
-            <button type="submit">Sign Up</button>
-        </form>
-    );
-};
 
-export default SignUpForm;
+        <div className="min-h-screen flex items-center justify-center bg-gray-100 py-12 px-4 sm:px-6 lg:px-8">
+            <div className="max-w-md w-full">
+                <div>
+                    <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">
+                        Create your account
+                    </h2>
+                </div>
+                <input
+                    type="text"
+                    className="mt-2 block w-full p-3 rounded-md bg-gray-200 border-transparent focus:border-gray-500 focus:bg-white focus:ring-0"
+                    placeholder="Username"
+                    value={username}
+                    onChange={e => setUsername(e.target.value)}
+                />
+                <input
+                    type="password"
+                    className="mt-2 block w-full p-3 rounded-md bg-gray-200 border-transparent focus:border-gray-500 focus:bg-white focus:ring-0"
+                    placeholder="Password"
+                    value={password}
+                    onChange={e => setPassword(e.target.value)}
+                />
+                <button className="mt-2 w-full px-3 py-4 text-white bg-indigo-500 rounded-md focus:bg-indigo-600 focus:outline-none" onClick={handleSignUp}>
+                    Sign Up
+                </button>
+            </div>
+        </div>
+    );
+}
+
+export default SignUpPage;
