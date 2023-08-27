eda7481020e18b6a06b8f740202bbe050f909dad
barthollomew <nathan.nn946@gmail.com>
2023-08-27 18:04:07 +1000

    Added working movie times and dates

--- src/components/Movies/Movies.js
+++ src/components/Movies/Movies.js
@@ -1,113 +1,191 @@
-import React, { useState } from 'react';
-import { Link, useNavigate } from 'react-router-dom'; 
-import './Movies.css';
-import ReviewModal from '../Reviews/ReviewModal';
+import React, { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import ReviewModal from "../Reviews/ReviewModal";
+import "./Movies.css";
 
 const Movies = () => {
-    const [selectedDay, setSelectedDay] = useState(null);
-    const [selectedCinema, setSelectedCinema] = useState(null);
+  const [selectedDay, setSelectedDay] = useState(null);
+  const [selectedCinema, setSelectedCinema] = useState(null);
+  const [showReviewModal, setShowReviewModal] = useState(false);
+  const [currentMovieForReview, setCurrentMovieForReview] = useState("");
 
-    const [showReviewModal, setShowReviewModal] = useState(false);
-    const [currentMovieForReview, setCurrentMovieForReview] = useState('');
+  const handleOpenReview = (movieTitle) => {
+    setCurrentMovieForReview(movieTitle);
+    setShowReviewModal(true);
+  };
 
-    const handleOpenReview = (movieTitle) => {
-        setCurrentMovieForReview(movieTitle);
-        setShowReviewModal(true);
-    };
+  const isUserAuthenticated = () => {
+    const currentUser = JSON.parse(localStorage.getItem("currentUser") || "{}");
+    return Boolean(currentUser.name);
+  };
 
-    const isUserAuthenticated = () => {
-        const currentUser = JSON.parse(localStorage.getItem('currentUser') || '{}');
-        return Boolean(currentUser.name); 
+  const navigate = useNavigate();
+
+  const handleReviewClick = (movieTitle) => {
+    if (isUserAuthenticated()) {
+      handleOpenReview(movieTitle);
+    } else {
+      navigate("/SignIn");
     }
+  };
 
-    const navigate = useNavigate();
+  const days = [
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+    "Sunday",
+  ];
+  const cinemas = ["Williamstown", "Melbourne Central", "West Footscray"];
 
-    const handleReviewClick = (movieTitle) => {
-        if (isUserAuthenticated()) {
-            handleOpenReview(movieTitle);
-        } else {
-            navigate("/SignIn");
-        }
-    }
+  const movies = [
+    {
+      title: "Shrek 1",
+      imgSrc: "shrek1.jpg",
+      showings: {
+        Monday: {
+          Williamstown: ["10:00", "14:00"],
 
-    
-    const days = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"];
-    const cinemas = ["Cinema 1", "Cinema 2", "Cinema 3"];
-    const movies = [
-        {
-            title: "Movie 1",
-            sessionTimes: ["10:00", "12:30", "15:00", "18:00"],
-            imgSrc: "path/to/placeholder/image1.png"
+          "West Footscray": ["12:00", "18:00"],
         },
-        {
-            title: "Movie 2",
-            sessionTimes: ["11:00", "13:30", "16:00", "19:00"],
-            imgSrc: "path/to/placeholder/image2.png"
-        }
-    ];
+        Wednesday: { "Melbourne Central": ["10:00", "16:00"] },
+        Saturday: { "West Footscray": ["11:00", "15:00"] },
+      },
+    },
+    {
+      title: "Shrek 2",
+      imgSrc: "shrek2.jpg",
+      showings: {
+        Tuesday: { "Melbourne Central": ["10:00", "16:00", "20:00"] },
+        Thursday: { Williamstown: ["09:00", "12:00"] },
+        Sunday: { "West Footscray": ["12:00", "17:00"] },
+      },
+    },
+    {
+      title: "Shrek 3",
+      imgSrc: "shrek3.jpg",
+      showings: {
+        Monday: {
+          Williamstown: ["11:00", "15:00"],
+          "Melbourne Central": ["14:00"],
+        },
+        Friday: { "West Footscray": ["10:00", "14:00", "18:00"] },
+        Sunday: { "Melbourne Central": ["13:00", "17:00"] },
+      },
+    },
+    {
+      title: "Shrek 4",
+      imgSrc: "shrek4.jpg",
+      showings: {
+        Tuesday: { "West Footscray": ["12:00", "16:00", "20:00"] },
+        Wednesday: { "Melbourne Central": ["09:00", "13:00"] },
+        Saturday: { Williamstown: ["11:00", "15:00"] },
+      },
+    },
+    {
+      title: "Kek",
+      imgSrc: "shrekek.jpg",
+      showings: {
+        Tuesday: { "Melbourne Central": ["12:00", "15:00", "19:00"] },
+        Friday: { Williamstown: ["13:00", "18:00"] },
+        Sunday: { "West Footscray": ["11:00", "16:00"] },
+      },
+    },
+  ];
 
-    return (
-        <div className="pt-16 pb-16 text-center">
-            <div className="movies-container p-8">
-                <div className="days flex gap-4 mb-8">
-                    {days.map(day => (
-                        <button
-                            key={day}
-                            className={`day ${selectedDay === day ? 'bg-blue-500 text-white' : 'bg-gray-200'}`}
-                            onClick={() => {
-                                setSelectedDay(day);
-                                setSelectedCinema(null);  
-                            }}
-                        >
-                            {day}
-                        </button>
-                    ))}
-                </div>
+  const filteredMovies = movies.filter(
+    (movie) => movie.showings[selectedDay]?.[selectedCinema]
+  );
 
-                {selectedDay && (
-                    <div className="cinemas flex gap-4 mb-8">
-                        {cinemas.map(cinema => (
-                            <button
-                                key={cinema}
-                                className={`cinema ${selectedCinema === cinema ? 'bg-blue-500 text-white' : 'bg-gray-200'}`}
-                                onClick={() => setSelectedCinema(cinema)}
-                            >
-                                {cinema}
-                            </button>
-                        ))}
+  return (
+    <div className="pt-16 pb-16 text-center">
+      <div className="max-w-screen-lg mx-auto p-8">
+        <div className="flex gap-4 mb-8">
+          {days.map((day) => (
+            <button
+              key={day}
+              className={`px-4 py-2 rounded ${
+                selectedDay === day ? "bg-blue-500 text-white" : "bg-gray-200"
+              } transition-colors duration-300`}
+              onClick={() => {
+                setSelectedDay(day);
+                setSelectedCinema(null);
+              }}
+            >
+              {day}
+            </button>
+          ))}
+        </div>
+
+        {selectedDay && (
+          <div className="flex gap-4 mb-8">
+            {cinemas.map((cinema) => (
+              <button
+                key={cinema}
+                className={`px-4 py-2 rounded ${
+                  selectedCinema === cinema
+                    ? "bg-blue-500 text-white"
+                    : "bg-gray-200"
+                } transition-colors duration-300`}
+                onClick={() => setSelectedCinema(cinema)}
+              >
+                {cinema}
+              </button>
+            ))}
+          </div>
+        )}
+
+        {selectedDay && selectedCinema && (
+          <div className="grid gap-4">
+            {filteredMovies.length > 0 ? (
+              filteredMovies.map((movie) => {
+                const showings = movie.showings[selectedDay][selectedCinema];
+                return (
+                  <div
+                    key={movie.title}
+                    className="p-4 bg-gray-200 rounded shadow-lg hover:shadow-md transition-shadow duration-300"
+                  >
+                    <img
+                      src={process.env.PUBLIC_URL + "/images/" + movie.imgSrc}
+                      alt={movie.title}
+                      className="mx-auto w-32 h-32 object-cover rounded mb-4"
+                    />
+                    <h2 className="text-xl font-bold mb-4">{movie.title}</h2>
+                    <div className="flex gap-4">
+                      {showings.map((time) => (
+                        <Link key={time} to="/SeatSelection">
+                          <span className="px-4 py-2 bg-blue-500 text-white rounded cursor-pointer hover:bg-blue-700 transition-colors duration-300">
+                            {time}
+                          </span>
+                        </Link>
+                      ))}
                     </div>
-                )}
+                    <button
+                      onClick={() => handleReviewClick(movie.title)}
+                      className="mt-2 bg-blue-500 text-white rounded px-4 py-2 hover:bg-blue-700 transition-colors duration-300"
+                    >
+                      Leave a Review
+                    </button>
+                  </div>
+                );
+              })
+            ) : (
+              <p>No movies showing on this day.</p>
+            )}
+          </div>
+        )}
 
-                    {selectedCinema && (
-                        <div className="movie-list grid gap-4">
-                            {movies.map(movie => (
-                                <div key={movie.title} className="movie p-4 bg-gray-200 rounded shadow">
-                                    <img src={movie.imgSrc} alt={movie.title} className="movie-image mb-4" />
-                                    <h2 className="movie-title mb-4">{movie.title}</h2>
-                                    <div className="session-times flex gap-4">
-                                        {movie.sessionTimes.map(time => (
-                                            <Link key={time} to="/SeatSelection">  
-                                                <span className="time p-2 bg-blue-500 text-white rounded cursor-pointer">
-                                                    {time}
-                                                </span>
-                                            </Link>
-                                        ))}
-                                    </div>
-                                    <button onClick={() => handleReviewClick(movie.title)}>Leave a Review</button>
-                                </div>
-                            ))}
-                        </div>
-                    )}
-                    {/* Review Modal */}
-                    {showReviewModal && (
-                        <ReviewModal 
-                            movieTitle={currentMovieForReview} 
-                            onClose={() => setShowReviewModal(false)} 
-                        />
-                    )}
-            </div>
-        </div>       
-    );
-}
+        {showReviewModal && (
+          <ReviewModal
+            movieTitle={currentMovieForReview}
+            onClose={() => setShowReviewModal(false)}
+          />
+        )}
+      </div>
+    </div>
+  );
+};
 
 export default Movies;
