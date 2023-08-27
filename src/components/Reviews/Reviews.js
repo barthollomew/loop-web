ca9b70a7dc02efe0e7cab177ac2044810953d52f
barthollomew <nathan.nn946@gmail.com>
2023-08-27 18:18:58 +1000

    Added review login validation

--- src/components/Reviews/Reviews.js
+++ src/components/Reviews/Reviews.js
@@ -1,65 +1,88 @@
-// Reviews.js
-
 import React, { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
 import './Reviews.css';
 
 export const deleteUserReviews = (userName) => {
-    const allReviews = JSON.parse(localStorage.getItem('reviews') || '[]');
-    const updatedReviews = allReviews.filter(review => review.userName !== userName);
-    localStorage.setItem('reviews', JSON.stringify(updatedReviews));
-}
+  const allReviews = JSON.parse(localStorage.getItem('reviews') || '[]');
+  const updatedReviews = allReviews.filter((review) => review.userName !== userName);
+  localStorage.setItem('reviews', JSON.stringify(updatedReviews));
+};
 
 const Reviews = () => {
-    const [reviews, setReviews] = useState([]);
+  const [reviews, setReviews] = useState([]);
+  const currentUser = JSON.parse(localStorage.getItem('currentUser') || '{}');
 
-    const handleDeleteReview = (index) => {
-        const updatedReviews = [...reviews];
-        updatedReviews.splice(index, 1);
-        setReviews(updatedReviews);
-        localStorage.setItem('reviews', JSON.stringify(updatedReviews));
+  const handleDeleteReview = (index) => {
+    const updatedReviews = [...reviews];
+    updatedReviews.splice(index, 1);
+    setReviews(updatedReviews);
+    localStorage.setItem('reviews', JSON.stringify(updatedReviews));
+  };
+
+  const handleEditReview = (index) => {
+    const newComment = prompt('Edit your comment:', reviews[index].comment);
+    if (newComment) {
+      const updatedReviews = [...reviews];
+      updatedReviews[index].comment = newComment;
+      setReviews(updatedReviews);
+      localStorage.setItem('reviews', JSON.stringify(updatedReviews));
     }
+  };
 
-    const handleEditReview = (index) => {
-        // For simplicity, we will just prompt for a new comment.
-        const newComment = prompt("Edit your comment:", reviews[index].comment);
-        if (newComment) {
-            const updatedReviews = [...reviews];
-            updatedReviews[index].comment = newComment;
-            setReviews(updatedReviews);
-            localStorage.setItem('reviews', JSON.stringify(updatedReviews));
-        }
-    }
+  useEffect(() => {
+    const storedReviews = JSON.parse(localStorage.getItem('reviews') || '[]');
+    setReviews(storedReviews);
+  }, []);
 
-    useEffect(() => {
-        const storedReviews = JSON.parse(localStorage.getItem('reviews') || '[]');
-        setReviews(storedReviews);
-    }, []);
-
-    return (
-        <div className="reviews-container">
-            <h2>User Reviews</h2>
-            {reviews.map((review, index) => (
-                <div key={index} className="review-card">
-                    <h3>{review.movieTitle}</h3>
-                    <p>By: {review.userName}</p>
-                    <div className="stars">
-                        {[1, 2, 3, 4, 5].map(star => (
-                            <span key={star}>
-                                {star <= review.rating ? '★' : '☆'}
-                            </span>
-                        ))}
-                    </div>
-                    <p>{review.comment}</p>
-                    {JSON.parse(localStorage.getItem('currentUser')).name === review.userName && (
-                        <>
-                            <button onClick={() => handleEditReview(index)}>Edit</button>
-                            <button onClick={() => handleDeleteReview(index)}>Delete</button>
-                        </>
-                    )}
-                </div>
-            ))}
+  return (
+    <div className="reviews-container">
+      <h2 className="text-2xl font-bold mb-4">User Reviews</h2>
+      {currentUser.name ? (
+        // Existing user can view and edit reviews
+        reviews.map((review, index) => (
+          <div key={index} className="review-card mb-4 p-4 border rounded">
+            <h3 className="text-lg font-semibold">{review.movieTitle}</h3>
+            <p className="text-sm text-gray-500">By: {review.userName}</p>
+            <div className="stars my-2">
+              {[1, 2, 3, 4, 5].map((star) => (
+                <span key={star} className="mr-1">
+                  {star <= review.rating ? '★' : '☆'}
+                </span>
+              ))}
+            </div>
+            <p>{review.comment}</p>
+            {currentUser.name === review.userName && (
+              <>
+                <button
+                  className="text-blue-500 hover:underline"
+                  onClick={() => handleEditReview(index)}
+                >
+                  Edit
+                </button>
+                <button
+                  className="text-red-500 hover:underline ml-4"
+                  onClick={() => handleDeleteReview(index)}
+                >
+                  Delete
+                </button>
+              </>
+            )}
+          </div>
+        ))
+      ) : (
+        // New user or not signed in
+        <div className="mt-4">
+          <p>Sign up to post a review!</p>
+          <Link
+            to="/SignUp"
+            className="inline-block mt-2 px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600"
+          >
+            Sign Up
+          </Link>
         </div>
-    );
-}
+      )}
+    </div>
+  );
+};
 
 export default Reviews;
