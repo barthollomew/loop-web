dab6181c3e30ce6ace973cfb472b379d6483773b
barthollomew <nathan.nn946@gmail.com>
2023-10-14 17:38:26 +1100

    Clean install

---
+++ newLoopWeb/backend/__tests__/backend.test.js
@@ -0,0 +1,82 @@
+const tap = require('tap');
+const { build } = require('../app');  // Import your Fastify creation logic
+
+tap.test('Successfully create an account', async (t) => {
+    const fastify = build();
+
+    const response = await fastify.inject({
+        method: 'POST',
+        url: '/api/accounts/signup',
+        payload: {
+            username: 'TestUser',
+            password: 'SecureP@ssw0rd',
+            email: 'testuser@example.com'
+        }
+    });
+
+    t.equal(response.statusCode, 201);
+    t.equal(response.json().message, 'Account created!');
+});
+tap.test('Fail to create an account with an existing username', async (t) => {
+    const fastify = build();
+
+    const response = await fastify.inject({
+        method: 'POST',
+        url: '/api/accounts/signup',
+        payload: {
+            username: 'ExistingUser',
+            password: 'SecureP@ssw0rd',
+            email: 'newuser@example.com'
+        }
+    });
+
+    t.equal(response.statusCode, 500);
+    t.match(response.json().message, /Error creating account/);
+});
+tap.test('Successfully sign in to an account', async (t) => {
+    const fastify = build();
+
+    const response = await fastify.inject({
+        method: 'POST',
+        url: '/api/accounts/signin',
+        payload: {
+            username: 'TestUser',
+            password: 'SecureP@ssw0rd'
+        }
+    });
+
+    t.equal(response.statusCode, 200);
+    t.equal(response.json().message, 'Sign in successful');
+});
+tap.test('Fail to sign in with incorrect password', async (t) => {
+    const fastify = build();
+
+    const response = await fastify.inject({
+        method: 'POST',
+        url: '/api/accounts/signin',
+        payload: {
+            username: 'TestUser',
+            password: 'WrongP@ssw0rd'
+        }
+    });
+
+    t.equal(response.statusCode, 401);
+    t.equal(response.json().message, 'Invalid credentials');
+});
+tap.test('Successfully update account details', async (t) => {
+    const fastify = build();
+
+    // Assume userId = 1 for the test user. This should be adjusted as per your DB.
+    const response = await fastify.inject({
+        method: 'PUT',
+        url: '/api/accounts/update/1',
+        payload: {
+            username: 'UpdatedUser',
+            password: 'NewP@ssw0rd',
+            email: 'updateduser@example.com'
+        }
+    });
+
+    t.equal(response.statusCode, 200);
+    t.equal(response.json().message, 'Account updated!');
+});
--- newLoopWeb/backend/app.js
+++ newLoopWeb/backend/app.js
@@ -1,6 +1,6 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const cors = require('cors');
+// Import Fastify and plugins
+const fastify = require('fastify')({ logger: true });
+const fastifyCors = require('fastify-cors');
 
 // Routers
 const accountsRouter = require('./routes/accounts');
@@ -8,22 +8,30 @@ const moviesRouter = require('./routes/movies');
 const reviewsRouter = require('./routes/reviews');
 const showtimesRouter = require('./routes/showtimes');
 
-const app = express();
-
 // Middlewares
-app.use(bodyParser.json());
-app.use(cors());
+fastify.register(fastifyCors); // This plugin adds a simple cors() utility
 
 // Routes
-app.use('/api/accounts', accountsRouter);
-app.use('/api/movies', moviesRouter);
-app.use('/api/reviews', reviewsRouter);
-app.use('/api/showtimes', showtimesRouter);
+fastify.register(accountsRouter, { prefix: '/api/accounts' });
+fastify.register(moviesRouter, { prefix: '/api/movies' });
+fastify.register(reviewsRouter, { prefix: '/api/reviews' });
+fastify.register(showtimesRouter, { prefix: '/api/showtimes' });
 
 // Server
-const PORT = process.env.PORT || 3001;
-if (process.env.NODE_ENV !== 'test') {
-    app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
-}
+const start = async () => {
+  try {
+    await fastify.listen(process.env.PORT || 3001);
+    fastify.log.info(`Server running on port ${fastify.server.address().port}`);
+  } catch (err) {
+    fastify.log.error(err);
+    process.exit(1);
+  }
+};
 
-module.exports = app;
+// Check if file is being required or run directly
+if (require.main === module) {
+    start();
+  } else {
+    module.exports = { build: fastify };
+  }
+  
